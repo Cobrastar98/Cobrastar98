e1a9e2985c92783fe3ab28ad5063872f4423ba20
Cobrastar98 <46357721+Cobrastar98@users.noreply.github.com>
2022-06-04 15:16:09 -0500

    Update csvconvert.py

--- csvconvert.py
+++ csvconvert.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# NOTE: I am not the original author of this script. However, I modified line 17 so it would correctly process my raw SAP log files.
+
 import csv
 
 ii = 0
